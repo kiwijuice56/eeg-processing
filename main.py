bb1fc101b1132698f2fdf7f2f7ec922e4149c3ca
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-21 16:44:21 -0400

    Importing JSON data from Godot

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import numpy as np
 import scipy
+import json
 
-print("hello world!")
+import re
+
+raw_data = {}
+
+with open("data/test_eeg_and_psd.json") as f:
+    raw_json_data = str(f.readline())
+
+    # Python's json module crashes if the file contains 'nan' rather than 'NaN'
+    raw_json_data = re.sub(r'\bnan\b', 'NaN', raw_json_data)
+
+    raw_data = json.loads(raw_json_data)
