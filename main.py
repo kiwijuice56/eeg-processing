4deb218639e0d9ef2c3921792c8e544ef667fb40
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-21 16:57:46 -0400

    Basic plotting of data

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 import scipy
-import json
 
+import matplotlib.pyplot as plt
+
+import json
 import re
 
 raw_data = {}
@@ -13,3 +15,11 @@ with open("data/test_eeg_and_psd.json") as f:
     raw_json_data = re.sub(r'\bnan\b', 'NaN', raw_json_data)
 
     raw_data = json.loads(raw_json_data)
+
+
+raw_alpha_time = np.array(raw_data["alpha_absolute"]["time"])
+raw_alpha_value = np.transpose(np.array(raw_data["alpha_absolute"]["value"]))
+
+plt.plot(raw_alpha_time, raw_alpha_value[1]) # left_forehead
+plt.plot(raw_alpha_time, raw_alpha_value[2]) # right_forehead
+plt.show()
