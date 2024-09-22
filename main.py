f9de8e1c3b505de7b9c8da2d6da5d12d3eb2ecdc
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-21 23:13:57 -0400

    Progress on replicating alpha

--- main.py
+++ main.py
@@ -40,9 +40,6 @@ raw_alpha_value = np.array(raw_data["alpha_absolute"]["value"])
 raw_eeg_time = np.array(raw_data["eeg"]["time"])
 raw_eeg_value = np.array(raw_data["eeg"]["value"])
 
-print(len(raw_eeg_value))
-print(len(raw_alpha_value))
-
 # Interpolate data with consistent spacing between samples
 # (Channel 1 is left forehead)
 raw_eeg_time, raw_eeg_value = interpolate_signal(raw_eeg_time, raw_eeg_value[:, 1])
@@ -50,13 +47,28 @@ raw_alpha_time, raw_alpha_value = interpolate_signal(raw_alpha_time, raw_alpha_v
 
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.welch.html
 # https://github.com/HPI-CH/UNIVERSE/blob/main/Features/features.py
-freqs, psd = signal.welch(x=raw_eeg_value, fs=256.0, window="hann")
+# https://web.archive.org/web/20181105231756/http://developer.choosemuse.com/tools/available-data#Absolute_Band_Powers
+# https://mind-monitor.com/forums/viewtopic.php?t=1651
 
-bandpower = yasa.bandpower_from_psd_ndarray(psd, freqs) # Calculate the bandpower on 3-D PSD array
-bandpower = np.transpose(bandpower, (1, 2, 0))
+window_size = 256
+window_gap = 25
 
-print(bandpower)
+test_alpha_time = []
+test_alpha_value = []
+
+for i in range(window_size, len(raw_eeg_time), window_gap):
+    window = raw_eeg_value[i - window_size : i]
+    if len(window) < window_size:
+        break
+    freqs, psd = signal.welch(x=window, fs=256.0, window="hann")
+    bandpower = yasa.bandpower_from_psd_ndarray(psd, freqs) # Calculate the bandpower on 3-D PSD array
+
+    test_alpha_time.append(raw_eeg_time[i])
+    test_alpha_value.append(bandpower[1])
+
+print(len(test_alpha_time))
+print(len(raw_alpha_time))
 
 plt.plot(raw_alpha_time, raw_alpha_value)
-
+plt.plot(test_alpha_time, test_alpha_value)
 plt.show()
