c8324d50b6c1536a3708c75974c418af5b80be8e
kiwijuice56 <alfaroeric127@gmail.com>
2024-09-23 14:52:44 -0400

    Yasa's method

--- bandpower.py
+++ bandpower.py
@@ -1,11 +1,64 @@
 import numpy as np
-from scipy import signal
-import yasa
+import scipy
 
 import matplotlib.pyplot as plt
 
 
 # Calculates bandpower of different frequencies in EEG data
+
+
+# https://raphaelvallat.com/bandpower.html
+def bandpower(data, sf, band, window_sec=None, relative=False):
+    """Compute the average power of the signal x in a specific frequency band.
+
+    Parameters
+    ----------
+    data : 1d-array
+        Input signal in the time-domain.
+    sf : float
+        Sampling frequency of the data.
+    band : list
+        Lower and upper frequencies of the band of interest.
+    window_sec : float
+        Length of each window in seconds.
+        If None, window_sec = (1 / min(band)) * 2
+    relative : boolean
+        If True, return the relative power (= divided by the total power of the signal).
+        If False (default), return the absolute power.
+
+    Return
+    ------
+    bp : float
+        Absolute or relative band power.
+    """
+    from scipy.signal import welch
+    from scipy.integrate import simpson
+    band = np.asarray(band)
+    low, high = band
+
+    # Define window length
+    if window_sec is not None:
+        nperseg = window_sec * sf
+    else:
+        nperseg = (2 / low) * sf
+
+    # Compute the modified periodogram (Welch)
+    freqs, psd = welch(data, sf, nperseg=nperseg)
+
+    # Frequency resolution
+    freq_res = freqs[1] - freqs[0]
+
+    # Find closest indices of band in frequency vector
+    idx_band = np.logical_and(freqs >= low, freqs <= high)
+
+    # Integral approximation of the spectrum using Simpson's rule.
+    bp = simpson(psd[idx_band], dx=freq_res)
+
+    bp = np.log(bp)
+
+    if relative:
+        bp /= simpson(psd, dx=freq_res)
+    return bp
 
 
 raw_alpha_time = np.fromfile("data/raw_alpha_time.npy")
@@ -19,21 +72,20 @@ raw_eeg_value = np.fromfile("data/raw_eeg_value.npy")
 # https://mind-monitor.com/forums/viewtopic.php?t=1651
 
 window_size = 256
-window_gap = 25
+window_gap = 26
 
 test_alpha_time = []
 test_alpha_value = []
 
 for i in range(window_size, len(raw_eeg_time), window_gap):
-    window = raw_eeg_value[i - window_size : i]
-    if len(window) < window_size:
-        break
-    freqs, psd = signal.welch(x=window, fs=256.0, window="hann")
-    bandpower = yasa.bandpower_from_psd_ndarray(psd, freqs) # Calculate the bandpower on 3-D PSD array
+    window_value = raw_eeg_value[i - window_size : i]
 
     test_alpha_time.append(raw_eeg_time[i])
-    test_alpha_value.append(bandpower[1])
+    test_alpha_value.append(bandpower(window_value, 256.0, [7.5, 13.0]))
 
+# This is slightly incorrect; the interpolated alpha signals start at t = 0,
+# but they actually have a short delay before Muse sends any packets.
+# In any case, this is still useful to see if the overall pattern is the same
 plt.plot(raw_alpha_time, raw_alpha_value)
 plt.plot(test_alpha_time, test_alpha_value)
 plt.show()
