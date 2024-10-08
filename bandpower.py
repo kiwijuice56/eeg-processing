af5706c030c58f4e27dd7402cc61665d90f62d18
kiwijuice56 <alfaroeric127@gmail.com>
2024-10-08 11:20:57 -0400

    Much more accurate alpha signal

--- bandpower.py
+++ bandpower.py
@@ -18,21 +18,21 @@ def bandpower(data, sampling_freq, band):
 raw_alpha_time = np.fromfile("data/raw_alpha_time.npy")
 raw_alpha_value = np.fromfile("data/raw_alpha_value.npy")
 raw_eeg_time = np.fromfile("data/raw_eeg_time.npy")
-raw_eeg_value = np.fromfile("data/raw_eeg_value.npy")
+eeg_value = np.fromfile("data/filtered_eeg_value.npy")
 
 # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.welch.html
 # https://github.com/HPI-CH/UNIVERSE/blob/main/Features/features.py
 # https://web.archive.org/web/20181105231756/http://developer.choosemuse.com/tools/available-data#Absolute_Band_Powers
 # https://mind-monitor.com/forums/viewtopic.php?t=1651
 
-window_size = 1024
-window_gap = 128
+window_size = 2048
+window_gap = 256
 
 test_alpha_time = []
 test_alpha_value = []
 
 for i in range(window_size, len(raw_eeg_time), window_gap):
-    window_value = raw_eeg_value[i - window_size : i]
+    window_value = eeg_value[i - window_size : i]
 
     test_alpha_time.append(raw_eeg_time[i])
     test_alpha_value.append(bandpower(window_value, 1024.0, [7.5, 13.0]))
--- filter.py
+++ filter.py
@@ -6,8 +6,15 @@ import matplotlib.pyplot as plt
 raw_eeg_time = np.fromfile("data/raw_eeg_time.npy")
 raw_eeg_value = np.fromfile("data/raw_eeg_value.npy")
 
+# Normalize the data
+raw_eeg_value -= np.median(raw_eeg_value)
+raw_eeg_value /= np.std(raw_eeg_value)
+
 # Remove powerline interference (Anders)
 filtered_eeg_value = mne.filter.filter_data(raw_eeg_value, 1024, 0.5, 50)
+
+# Save filtered signal
+filtered_eeg_value.tofile("data/filtered_eeg_value.npy")
 
 plt.plot(raw_eeg_time[0:1024], raw_eeg_value[0:1024])
 plt.plot(raw_eeg_time[0:1024], filtered_eeg_value[0:1024])
